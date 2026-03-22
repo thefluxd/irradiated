8534e084bd5be9476f0bfad7058ee8e3526c533d
fluxd <thefluxd@proton.me>
2026-03-22 14:52:49 +0100

    add home page url

--- TODO.md
+++ TODO.md
@@ -2,4 +2,3 @@
 
 - change radiation particles
 - amplify radiation farther from border, cuz u can survive whin golden apples
-- add open page in about command
--- src/main/java/net/fluxd/irradiated/commands/commands/ModCommand.java
+++ src/main/java/net/fluxd/irradiated/commands/commands/ModCommand.java
@@ -1,5 +1,7 @@
 package net.fluxd.irradiated.commands.commands;
 
+import java.net.URL;
+
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.mojang.brigadier.context.CommandContext;
 
@@ -9,7 +11,9 @@ import net.fluxd.irradiated.commands.commands.modcommand.ReloadCommand;
 import net.minecraft.ChatFormatting;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
+import net.minecraft.network.chat.ClickEvent;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.HoverEvent;
 import net.minecraftforge.fml.ModList;
 import net.minecraftforge.forgespi.language.IModInfo;
 
@@ -34,19 +38,23 @@ public class ModCommand implements ICommand {
     String name = modInfo.getDisplayName();
     String version = modInfo.getVersion().toString();
     String authors = modInfo.getConfig().getConfigElement("authors").map(Object::toString).orElse("Unknown");
+    String url = modInfo.getModURL().map(URL::toString).orElse("No URL provided");
 
     Component message = Component.empty()
         .append(Component.literal("--- ").withStyle(ChatFormatting.GRAY))
         .append(Component.literal(name).withStyle(ChatFormatting.WHITE))
         .append(Component.literal(" ---").withStyle(ChatFormatting.GRAY))
         .append("\n")
-        .append(Component.literal("Version: ").withStyle(ChatFormatting.YELLOW))
+        .append(Component.literal("Version: ").withStyle(ChatFormatting.GRAY))
         .append(Component.literal(version).withStyle(ChatFormatting.WHITE))
         .append("\n")
-        .append(Component.literal("Author: ").withStyle(ChatFormatting.YELLOW))
+        .append(Component.literal("Author: ").withStyle(ChatFormatting.GRAY))
         .append(Component.literal(authors).withStyle(ChatFormatting.WHITE))
-        .append("\n")
-        .append(Component.literal("fluxd on a server?!").withStyle(ChatFormatting.GRAY));
+        .append("\n").append(Component.literal("[Home page]").withStyle(style -> style
+            .withClickEvent(
+                new ClickEvent(ClickEvent.Action.OPEN_URL, url))
+            .withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, Component.literal("Opens mod's home page")))
+            .withUnderlined(true)));
 
     context.getSource().sendSuccess(() -> message, false);
     return 1;
